2434d6d00ef683a53140d0025ac599741e4f8275
AkhonaMj <akhona0304mjikelo@gmail.com>
2024-06-26 11:21:48 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@
 </template>
 
 <script>
-  import SignUp from '../components/SignUp.vue'
+  import SignUp from './components/SignUp.vue'
   export default {
       name: 'App',
       components: {
